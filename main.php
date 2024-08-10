e6fe1ab8bb94c0643cd806fdd6338fcd1f76b2b3
leofountas <leofoutas@gmail.com>
2024-08-11 01:10:00 +0200

    modal kinda finished even if could be better but at least is a good general idea ...need just to write the js to open and close modal

--- main.php
+++ main.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 
 if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
@@ -12,58 +12,61 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
     }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cattoflix</title>
-    <link rel="stylesheet" href="assets/mainstyle.css"> 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="icon"  href="assets\images\minilogo.png" type="image/icon">
-  </head>
-  <body>
-    <header>
-        <nav>
-            <div class="logodiv">
-                <img src="assets\images\logo.png">
-                <h1>CATTOFLIX</h1>
-                <p>Hello, <?php echo $_SESSION['username'];?></p>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Cattoflix</title>
+        <link rel="stylesheet" href="assets/mainstyle.css">
+        <link rel="stylesheet" href="assets/modal.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="icon" href="assets\images\minilogo.png" type="image/icon">
+    </head>
+
+    <body>
+        <header>
+            <nav>
+                <div class="logodiv">
+                    <img src="assets\images\logo.png">
+                    <h1>CATTOFLIX</h1>
+                    <p style="color: red;">Hello, <?php echo $_SESSION['username']; ?></p>
+                </div>
+                <div class="searchdiv">
+                    <input type="search" placeholder="Search for any movie">
+                    <!-- <button><i class="fa-solid fa-bars"></i></button> -->
+                </div>
+            </nav>
+        </header>
+        <aside>
+            <div class="sidebar-content1">
+                <!-- <img alt="logo" src="assets\images\logo.png"> -->
+                <h1>Genres</h1>
+                <ul>
+                    <li><a href="">Birds</a></li>
+                    <li><a href="">Fish</a></li>
+                    <li><a href="">Wild</a></li>
+                    <li><a href="">Cathub</a></li>
+                </ul>
             </div>
-            <div class="searchdiv">
-                <input type="search" placeholder="Search for any movie">
-                <!-- <button><i class="fa-solid fa-bars"></i></button> -->
+            <div class="sidebar-content2">
+                <h1>Options</h1>
+                <ul>
+                    <li><a href="">Something</a></li>
+                    <li><a href="">Something</a></li>
+                    <li><a href="">Something</a></li>
+                    <li><a href="">Something</a></li>
+                </ul>
             </div>
-        </nav>
-    </header>
-    <aside>
-        <div class="sidebar-content1">
-            <!-- <img alt="logo" src="assets\images\logo.png"> -->
-            <h1>Genres</h1>
-            <ul>
-                <li><a href="">Birds</a></li>
-                <li><a href="">Fish</a></li>
-                <li><a href="">Wild</a></li>
-                <li><a href="">Cathub</a></li>
-            </ul>
-        </div>
-        <div class="sidebar-content2">
-            <h1>Options</h1>
-            <ul>
-                <li><a href="">Something</a></li>
-                <li><a href="">Something</a></li>
-                <li><a href="">Something</a></li>
-                <li><a href="">Something</a></li>
-            </ul>
-        </div>
-        <div class="logout-container">
-            <form action="" method="post"><button type="submit" name="logout" class="logout"><i class="fas fa-sign-out-alt"></i> Logout</button></form>
-        </div>
-    </aside>
-    <!-- <div> if u want the space to the right side just re-active this div-->
+            <div class="logout-container">
+                <form action="" method="post"><button type="submit" name="logout" class="logout"><i class="fas fa-sign-out-alt"></i> Logout</button></form>
+            </div>
+        </aside>
+        <!-- <div> if u want the space to the right side just re-active this div-->
         <div class="featured">
             <div class="featured-content">
-                <img src="https://via.placeholder.com/300x100?text=MONEY+HEIST" alt="logo" class="logo"/>
+                <img src="https://via.placeholder.com/300x100?text=MONEY+HEIST" alt="logo" class="logo" />
                 <div>
                     <span class="rating">IMDb 8.8/10</span>
                     <span class="streams">2B+ Streams</span>
@@ -89,8 +92,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
                     <div class="item" style="--position: 9"><img src="https://via.placeholder.com/200x300?text=Movie+1" alt=""></div>
                 </div>
             </div>
-            
-            <h2 id="titlecarousel2"class="row-title">New this week</h2>
+
+            <h2 id="titlecarousel2" class="row-title">New this week</h2>
             <div id="carousel2" class="slider" reverse="true" style="
                 --width: 200px;
                 --height: 200px;
@@ -126,8 +129,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
                     <div class="item" style="--position: 9"><img src="https://via.placeholder.com/200x300?text=Movie+1" alt=""></div>
                 </div>
             </div>
-            
-            <h2 id="titlecarousel2"class="row-title">New this week</h2>
+
+            <h2 id="titlecarousel2" class="row-title">New this week</h2>
             <div id="carousel2" class="slider" reverse="true" style="
                 --width: 200px;
                 --height: 200px;
@@ -146,10 +149,66 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
                 </div>
             </div>
         </main>
-      </div>
-    <!-- </div> -->
-  </body>
-</html>
-<?php }else {
+        </div>
+        <!-- </div> -->
+
+        <!-- modal -->
+        <div class="modal">
+            <button class="close-modal">X</button>
+            <div class="modal-header">
+                <div class="modal-video">
+                <iframe class="video-player" src="https://www.youtube.com/embed/yySWoxNgoQM?si=uZ7CAPVi3NlewpWd&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                </div>
+                <div class="modal-descriptions">
+                    <h1 class="title">Insert title</h1>
+                    <p class="author">channel name or author name</p>
+                    <p>Here's the description of the videosdasd sdasda dasdsd adsad as sdsds sdsd sdsa dd asdsa dsadsdasd sadsa sdadsdsaaaaaaaaaaaaaaaa saaaaa. Here's the description of the videosdasd sdasda dasdsd adsad as sdsds sdsd sdsa dd asdsa dsadsdasd sadsa sdadsdsaaaw</p>
+                </div>
+            </div>
+            <div class="modal-body">
+                <h2>Comments</h2>
+                <form action="" method="post">
+                <input type="text" name="message" placeholder="Leave a comment here">
+                <button type="submit" name="send" class="send">Send</button>
+                </form>
+                    
+                <div class="modal-comments">
+
+                    <!-- start example of comment...we will generate them with php from the database so I know what I did is usless u.u-->
+                     <?php if(isset($_POST['send'])) {?>
+                    <div class="comment">
+                        <p class="date"><?=date('Y-m-d H:i:s');?></p>
+                        <h3><?=$_SESSION['username'];?>:</h3>
+                        <p class="message-txt"><?=$_POST['message'];?></p>
+                    </div>
+                    <div class="comment">
+                        <p class="date"><?=date('Y-m-d H:i:s');?></p>
+                        <h3><?=$_SESSION['username'];?>:</h3>
+                        <p class="message-txt"><?=$_POST['message'];?></p>
+                    </div>
+                    <div class="comment">
+                        <p class="date"><?=date('Y-m-d H:i:s');?></p>
+                        <h3><?=$_SESSION['username'];?>:</h3>
+                        <p class="message-txt"><?=$_POST['message'];?></p>
+                    </div>
+                    <div class="comment">
+                        <p class="date"><?=date('Y-m-d H:i:s');?></p>
+                        <h3><?=$_SESSION['username'];?>:</h3>
+                        <p class="message-txt"><?=$_POST['message'];?></p>
+                    </div>
+                    <div class="comment">
+                        <p class="date"><?=date('Y-m-d H:i:s');?></p>
+                        <h3><?=$_SESSION['username'];?>:</h3>
+                        <p class="message-txt"><?=$_POST['message'];?></p>
+                    </div>
+                    <?php }; ?>
+                    <!-- end comment generated -->
+
+                </div>
+            </div>
+        </div>
+    </body>
+    </html>
+<?php } else {
     header('Location: form.php');
 } ?>
